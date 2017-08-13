af82afe9d2768101c77bb21edde1f71fcbf622a6
Sam <samuel.messina@siu.edu>
2017-08-13 13:56:26 -0400

    Modify light and dark script to work with termite

--- scripts/scripts/dark.sh
+++ scripts/scripts/dark.sh
@@ -1,7 +1,16 @@
 rm /home/zookeeprr/dotfiles/x/.Xresources
+
 rm /home/zookeeprr/dotfiles/vim/.config/nvim/colors.vim 
+
+rm /home/zookeeprr/dotfiles/termite/.config/termite/config
+
 ln -s /home/zookeeprr/dotfiles/x/.Xresources.dark /home/zookeeprr/dotfiles/x/.Xresources
+
 ln -s /home/zookeeprr/dotfiles/vim/.config/nvim/darkcolors.vim /home/zookeeprr/dotfiles/vim/.config/nvim/colors.vim 
+
+ln -s /home/zookeeprr/dotfiles/termite/.config/termite/config.dark /home/zookeeprr/dotfiles/termite/.config/termite/config
+
 xrdb -merge /home/zookeeprr/.Xresources 
 xrdb -load /home/zookeeprr/.Xresources
-killall -s SIGHUP urxvt
+
+killall -USR1 termite
--- scripts/scripts/light.sh
+++ scripts/scripts/light.sh
@@ -1,7 +1,14 @@
 rm /home/zookeeprr/dotfiles/x/.Xresources
+
 rm /home/zookeeprr/dotfiles/vim/.config/nvim/colors.vim 
+
+rm /home/zookeeprr/dotfiles/termite/.config/termite/config
+
 ln -s /home/zookeeprr/dotfiles/x/.Xresources.light /home/zookeeprr/dotfiles/x/.Xresources
+
+ln -s /home/zookeeprr/dotfiles/termite/.config/termite/config.light /home/zookeeprr/dotfiles/termite/.config/termite/config
+
 ln -s /home/zookeeprr/dotfiles/vim/.config/nvim/lightcolors.vim /home/zookeeprr/dotfiles/vim/.config/nvim/colors.vim 
 xrdb -merge /home/zookeeprr/.Xresources 
 xrdb -load /home/zookeeprr/.Xresources
-killall -s SIGHUP urxvt
+killall -USR1 termite
