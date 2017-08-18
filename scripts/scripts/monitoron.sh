a4fea836420186fe266cd78668b62bf93589b7c9
Sam Messina <samuel.messina@gmail.com>
2017-08-18 16:03:46 -0500

    monitor goes on the left now

--- scripts/scripts/monitoron.sh
+++ scripts/scripts/monitoron.sh
@@ -5,5 +5,5 @@
 xrandr --newmode "1920x1080" 148.5 1920 2008 2052 2200 1080 1089 1095 1125 +hsync +vsync
 xrandr --addmode HDMI3 1920x1080
 
-xrandr --output HDMI3 --mode 1920x1080 --right-of LVDS1 --output LVDS1 --mode 1366x768 
+xrandr --output HDMI3 --mode 1920x1080 --left-of LVDS1 --output LVDS1 --mode 1366x768 
 sh ~/.fehbg
